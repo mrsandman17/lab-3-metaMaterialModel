9cb7353a8ea9814c68b0d0fedb96a5aeb5df630d
amitz <amitz@cs.huji.ac.il>
2021-01-04 10:29:19 +0200

    Remove matlab-model

--- LinearSimulator/par_c.m
+++
@@ -1,7 +0,0 @@
-function ABCD=par_c(f,value)
-%Parallel Capacitor to GND
-%ABCD=par_c(f,value)
-
-    ABCD=repmat([1 0 -99 1],length(f),1);
-    ABCD(:,3)=(1i.*f.*2.*pi.*value);
-
--- LinearSimulator/par_l.m
+++
@@ -1,7 +0,0 @@
-function ABCD=par_l(f,value)
-%Parallel inductor to GND
-%ABCD=par_l(f,value)
-
-    ABCD=repmat([1 0 -99 1],length(f),1);
-    ABCD(:,3)=1./(1i.*f.*2.*pi.*value);
-
